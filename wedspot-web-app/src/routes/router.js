415bc6b9119f4396ba68f0d21ce4976b1af5655e
Nurdien Adijaya <nurdienadijaya@gmail.com>
2021-08-23 19:43:24 +0700

    add search page

--- wedspot-web-app/src/routes/router.js
+++ wedspot-web-app/src/routes/router.js
@@ -1,6 +1,8 @@
 import { Switch, Route } from "react-router-dom";
+import Footer from "../component/footer/footer";
 import Header from "../component/header/header";
 import Homepage from "../pages/Homepage";
+import HomeSearch from "../pages/search/search1";
 import SearchPage from "../pages/search/search2";
 import DetailPage from "../pages/vendor/detail";
 import PackageDetail from "../pages/vendor/package";
@@ -8,12 +10,16 @@ import PackageDetail from "../pages/vendor/package";
 const Routers = () => {
   return (
     <div>
+      {window.location.pathname !== "/" &&
+      window.location.pathname !== "/search" ? (
+        <Header />
+      ) : null}
       <Switch>
         <Route exact path="/">
           <Homepage />
         </Route>
         <Route exact path="/search">
-          <SearchPage />
+          <HomeSearch />
         </Route>
         <Route exact path="/vendor/:id">
           <DetailPage />
@@ -23,9 +29,10 @@ const Routers = () => {
         </Route>
 
         <Route exact path="/*">
-          <Header />
+          <HomeSearch />
         </Route>
       </Switch>
+      {window.location.pathname !== "/" ? <Footer /> : null}
     </div>
   );
 };
