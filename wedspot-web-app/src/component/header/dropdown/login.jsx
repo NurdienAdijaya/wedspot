c1fe14560dd3e59bbc14f69e7487301398fb119e
hogie12 <hogi.mayai@gmail.com>
2021-08-27 17:17:04 +0700

    fixing login and register

--- wedspot-web-app/src/component/header/dropdown/login.jsx
+++ wedspot-web-app/src/component/header/dropdown/login.jsx
@@ -4,11 +4,8 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import { Avatar } from "@material-ui/core";
 import { Notifications } from "@material-ui/icons";
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
-=======
 import { useDispatch } from "react-redux";
->>>>>>> 9b0c065cc70dff54b01626eda33b400d63a38907
 
 export function Notif() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -20,11 +17,6 @@ export function Notif() {
   const handleClose = () => {
     setAnchorEl(null);
   };
-<<<<<<< HEAD
- 
-=======
-
->>>>>>> 9b0c065cc70dff54b01626eda33b400d63a38907
   return (
     <div>
       <Button
@@ -49,10 +41,6 @@ export function Notif() {
 
 export function AvatarIcon({ avatar }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
-<<<<<<< HEAD
-=======
-  const dispatch = useDispatch();
->>>>>>> 9b0c065cc70dff54b01626eda33b400d63a38907
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -84,15 +72,8 @@ export function AvatarIcon({ avatar }) {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-<<<<<<< HEAD
         <MenuItem onClick={handleClose}><Link to="/account">Profile</Link></MenuItem>
         <MenuItem onClick={handleLogout}>Logout</MenuItem>
-=======
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>
-          <p onClick={handleLogout}>Logout</p>
-        </MenuItem>
->>>>>>> 9b0c065cc70dff54b01626eda33b400d63a38907
       </Menu>
     </div>
   );
